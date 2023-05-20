610a1ba73f05cf44ce77a276278bbbb9194f7e45
nadia <67629937+nadiaverdha@users.noreply.github.com>
2023-05-20 20:28:52 +0200

    added relu activation

--- neuralnetwork.py
+++ neuralnetwork.py
@@ -320,6 +320,14 @@ def softmax_prime(x):
     return p * (1 - p)
 
 
+def relu(x):
+    return np.maximum(0, x)
+
+
+def relu_prime(x):
+    return np.where(x > 0, 1, 0)
+
+
 # loss function and its derivative
 
 
